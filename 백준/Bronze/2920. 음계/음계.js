765793963f5367515caaf1e1447fd2d2d80138de
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-06-17 21:40:11 +0900

    [Bronze II] Title: 음계, Time: 96 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim();
+
+
+if (input == "1 2 3 4 5 6 7 8") {
+    console.log("ascending");
+} else if (input == "8 7 6 5 4 3 2 1") {
+    console.log("descending");
+} else {
+    console.log("mixed");
+}
