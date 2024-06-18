d94744fccd7affb259c7cb1a3cf36f80c0686c65
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-06-18 16:55:05 +0900

    [Silver III] Title: 게으른 백곰, Time: 2980 ms, Memory: 49820 KB -BaekjoonHub

---
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -0,0 +1,22 @@
+N, K = map(int, input().split())
+ice = [0] * 1000001
+l = 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    ice[b] = a
+    l = max(l, b)
+ice = ice[: l + 1]
+
+m = 0
+i = 0
+sum = sum(ice[: K * 2 + 1])
+while True:
+    try:
+        m = max(m, sum)
+        sum += ice[i + K * 2 + 1]
+        sum -= ice[i]
+        i += 1
+    except:
+        m = max(m, sum)
+        break
+print(m)
