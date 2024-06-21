9de90c98a2ffdbdc59a1a8f45d65acc8ec0f9fdb
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-06-22 00:53:48 +0900

    [Bronze III] Title: ACM 호텔, Time: 112 ms, Memory: 9876 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
@@ -1,7 +1,7 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
-input.slice(1).forEach(function(x) {
+for (let x of input.slice(1)) {
     let seq = x.split(" ").map(Number);
-    console.log((((seq[2]-1)%seq[0])+1)*100 + Math.floor((seq[2]-1)/(seq[0])+1));
-})
+    console.log((((seq[2] - 1) % seq[0]) + 1) * 100 + Math.floor((seq[2]-1)/(seq[0]) + 1));
+}
