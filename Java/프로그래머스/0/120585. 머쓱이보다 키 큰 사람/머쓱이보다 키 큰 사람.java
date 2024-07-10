27e55a6e5c34a393d822726638f512c3bd373493
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-07-10 19:08:11 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int n : array) {
+            if (n > height) answer ++;
+        }
+        
+        return answer;
+    }
+}
