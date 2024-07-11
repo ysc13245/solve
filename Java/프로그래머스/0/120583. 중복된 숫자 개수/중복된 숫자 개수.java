39813d15ce9c94d5f0fd64fbeae55979a1b79c57
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-07-11 12:34:51 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ Java/프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,5 +1,10 @@
 class Solution {
     public int solution(int[] array, int n) {
+        if (array.length == 0 || array.length > 100) throw new IllegalArgumentException();
+        if (n<0 || n>1000) throw new IllegalArgumentException();
+        for (int e:array){
+            if (e<0 || e > 1000) throw new IllegalArgumentException();
+        }
         int answer = 0;
         for (int m : array) {
             answer += n == m ? 1 : 0;
