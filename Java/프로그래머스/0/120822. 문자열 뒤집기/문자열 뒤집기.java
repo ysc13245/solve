138ccd6a04d3f0769e3b707f3a88d15a90eb7435
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-07-12 13:57:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        return sb.reverse().toString();
+    }
+}
