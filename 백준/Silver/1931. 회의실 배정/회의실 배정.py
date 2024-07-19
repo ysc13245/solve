9d208b0c4ba9b9f9718173ec59b6869dd464d4cb
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-07-19 23:01:09 +0900

    [Silver I] Title: 회의실 배정, Time: 4188 ms, Memory: 58284 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,14 @@
+def solution():
+    schedule=[]
+    for _ in range(int(input())):
+        schedule.append(list(map(int,input().split())))
+    schedule.sort(key=lambda x: (x[1], x[0]))
+    t = 0
+    cnt = 1
+    for i, x in enumerate(schedule[1:]):
+        if schedule[t][1] <= x[0]:
+            cnt += 1
+            t = i+1
+    print(cnt)
+solution()
+ 
