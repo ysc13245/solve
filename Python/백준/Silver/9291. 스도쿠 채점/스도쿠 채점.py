0283437ef9681704ba16d9b927dde4c461046144
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-07-24 10:19:00 +0900

    [Silver IV] Title: 스도쿠 채점, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/9291. 스도쿠 채점/스도쿠 채점.py
+++ Python/백준/Silver/9291. 스도쿠 채점/스도쿠 채점.py
@@ -1,4 +1,9 @@
 def solution():
+    import sys
+    
+    input=sys.stdin.readline
+    print=sys.stdout.write
+    
     for i in range(int(input())):
         standard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         if i!=0: input()
@@ -6,17 +11,17 @@ def solution():
 
 
         if any(sorted(row) != standard for row in board):
-            print(f"Case {i+1}: INCORRECT")
+            print(f"Case {i+1}: INCORRECT\n")
             continue
 
         if any(sorted(board[row][col] for row in range(9)) != standard for col in range(9)):
-            print(f"Case {i+1}: INCORRECT")
+            print(f"Case {i+1}: INCORRECT\n")
             continue
 
         if any(sorted(board[r+i][c+j] for i in range(3) for j in range(3)) != standard for r in [0, 3, 6] for c in [0, 3, 6]):
-            print(f"Case {i+1}: INCORRECT")
+            print(f"Case {i+1}: INCORRECT\n")
             continue
 
-        print(f"Case {i+1}: CORRECT")
+        print(f"Case {i+1}: CORRECT\n")
 
 solution()
