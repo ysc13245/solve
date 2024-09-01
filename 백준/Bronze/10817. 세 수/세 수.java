4f37bb81c208d7fa1378f0ae74a0ca3e5ba06c11
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-09-02 00:36:55 +0900

    [Bronze III] Title: 세 수, Time: 108 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -5,11 +5,9 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int []arr = Arrays.stream(br.readLine().split(" "))
+        System.out.println(Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .sorted()
-                .toArray();
-
-        System.out.println(arr[1]);
+                .toArray()[1]);
     }
 }
