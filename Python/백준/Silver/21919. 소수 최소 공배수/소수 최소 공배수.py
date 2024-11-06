c294d8e1e3506436e07f7f71d7d69df849383072
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-11-06 18:25:18 +0900

    [Silver III] Title: 소수 최소 공배수, Time: 428 ms, Memory: 33240 KB -BaekjoonHub

---
+++ Python/백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+import math
+
+input()
+n = math.prod({i for i in map(int, input().split()) if is_prime(i)})
+print(n if n != 1 else -1)
