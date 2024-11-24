6bb1274e95616b2a7e7b168548ee26f414a765ac
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2024-11-24 10:46:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,11 +1,11 @@
 def sol():
-    seq = [1, 2]
+    seq = [1,2]
     n = int(input())
     if n == 1:
         print(1)
         return
     for _ in range(n - 2):
-        seq.append((seq[-1] + seq[-2]) % 10007)
+        seq.append((seq.pop(0)+seq[0]) % 10007)
     print(seq[-1])
     return
 
