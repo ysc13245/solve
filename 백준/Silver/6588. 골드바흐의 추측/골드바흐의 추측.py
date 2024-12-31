9e3016421c422b03f0d25e74433b464ae6775f19
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-01-01 03:08:02 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1180 ms, Memory: 57276 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,20 @@
+lis = map(int, open(0).readlines()[:-1])
+seq = [0] * 2 + [1] * (1000000 - 1)
+
+for i in range(2, 1001):
+    if seq[i]:
+        for j in range(i * i, 1000000, i):
+            seq[j] = 0
+
+primes = [i for i in range(3, 1000000) if seq[i]]
+answer = []
+
+for n in lis:
+    for pn in primes:
+        if seq[n - pn]:
+            answer.append(f"{n} = {pn} + {n - pn}")
+            break
+    else:
+        answer.append("Goldbach's conjecture is wrong.")
+
+print("\n".join(answer))
