6bdfde94cbb0eab991921f4b0edd14229f6ef4a9
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-07-09 22:20:40 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 5156 ms, Memory: 284708 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+parent = list(range(n + 1))
+rank = [0] * (n + 1)
+
+
+def find(x):
+    root = x
+    while parent[root] != root:
+        root = parent[root]
+
+    while parent[x] != root:
+        parent[x], x = root, parent[x]
+    return root
+
+
+def union(x, y):
+    root_x = find(x)
+    root_y = find(y)
+
+    if root_x == root_y:
+        return False
+
+    if rank[root_x] > rank[root_y]:
+        parent[root_y] = root_x
+    elif rank[root_x] < rank[root_y]:
+        parent[root_x] = root_y
+    else:
+        parent[root_y] = root_x
+        rank[root_x] += 1
+    return True
+
+
+graph = sorted([list(map(int, input().split())) for _ in range(m)], key=lambda x: x[2])
+summed = cnt = highest = 0
+
+for a, b, c in graph:
+    if union(a, b):
+        highest = max(highest, c)
+        summed += c
+        cnt += 1
+        import sys
+
+print(summed - highest)
