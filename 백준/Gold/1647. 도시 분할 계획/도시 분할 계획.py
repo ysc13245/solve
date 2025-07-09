0438178cde2668a729168f9e5ee461a5c43e8b05
ysc13245 <147698362+ysc13245@users.noreply.github.com>
2025-07-09 22:34:19 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2572 ms, Memory: 286660 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,47 +1,48 @@
-from calendar import c
+
 import sys
-
-input = sys.stdin.readline
-n, m = map(int, input().split())
-parent = list(range(n + 1))
-rank = [0] * (n + 1)
-
-
-def find(x):
-    root = x
-    while parent[root] != root:
-        root = parent[root]
-
-    while parent[x] != root:
-        parent[x], x = root, parent[x]
-    return root
+def sol():
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    parent = list(range(n + 1))
+    rank = [0] * (n + 1)
 
 
-def union(x, y):
-    root_x = find(x)
-    root_y = find(y)
+    def find(x):
+        root = x
+        while parent[root] != root:
+            root = parent[root]
 
-    if root_x == root_y:
-        return False
-
-    if rank[root_x] > rank[root_y]:
-        parent[root_y] = root_x
-    elif rank[root_x] < rank[root_y]:
-        parent[root_x] = root_y
-    else:
-        parent[root_y] = root_x
-        rank[root_x] += 1
-    return True
+        while parent[x] != root:
+            parent[x], x = root, parent[x]
+        return root
 
 
-graph = sorted([list(map(int, input().split())) for _ in range(m)], key=lambda x: x[2], reverse=True)
-summed = cnt = highest = 0
+    def union(x, y):
+        root_x = find(x)
+        root_y = find(y)
 
-while cnt < n - 1:
-    a, b, c = graph.pop()
-    if union(a, b):
-        summed += c
-        cnt += 1
-        highest = c
+        if root_x == root_y:
+            return False
 
-print(summed - highest)
+        if rank[root_x] > rank[root_y]:
+            parent[root_y] = root_x
+        elif rank[root_x] < rank[root_y]:
+            parent[root_x] = root_y
+        else:
+            parent[root_y] = root_x
+            rank[root_x] += 1
+        return True
+
+
+    graph = sorted([list(map(int, input().split())) for _ in range(m)], key=lambda x: x[2], reverse=True)
+    summed = cnt = highest = 0
+
+    while cnt < n - 1:
+        a, b, c = graph.pop()
+        if union(a, b):
+            summed += c
+            cnt += 1
+            highest = c
+
+    print(summed - highest)
+sol()
